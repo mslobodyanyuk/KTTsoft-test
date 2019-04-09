1eec1bc3af3c112bdd52c948cecce61b3c64c28f
maksim <smstepani4@gmail.com>
2019-04-09 16:00:04 +0300

    create_function() deprecated - replace with anonymous function

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -40,15 +40,9 @@ echo "<pre> dataArray =", var_dump($dataArray) ,"</pre>";
             // Sort the array with the data so that the branches was the first.
 /***********replace create_function with anonymous function*************************************************************************************************/
             /*
-
              usort($dataArray, create_function('$a,$b','if ((int)$a["parentId"]===(int)$b["parentId"]) return (int)$a["nodeId"]>(int)$b["nodeId"] ? 1 : -1;
                         return (int)$a["parentId"]>(int)$b["parentId"] ? 1 : -1;'));
             */
-
-
-/*usort($dataArray, function($a,$b) { if ((int)$a["parentId"]===(int)$b["parentId"]) return (int)$a["nodeId"]>(int)$b["nodeId"] ? 1 : -1;
-                                    return (int)$a["parentId"]>(int)$b["parentId"] ? 1 : -1;});*/
-
             $func = function($a,$b) { if ((int)$a['parentId']===(int)$b['parentId']) return (int)$a['nodeId']>(int)$b['nodeId'] ? 1 : -1;
                                         return (int)$a['parentId']>(int)$b['parentId'] ? 1 : -1;};
             usort($dataArray, $func);
